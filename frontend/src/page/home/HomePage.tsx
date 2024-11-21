63a47bcbc45b91f1f56dd310ff789e3073b382fe
johnnybinh <thebinhnguyen2703@gmail.com>
2024-11-21 20:03:13 +0700

    everything

--- frontend/src/page/home/HomePage.tsx
+++ frontend/src/page/home/HomePage.tsx
@@ -1,38 +1,11 @@
-import React, { useEffect, useState } from "react";
-import { logout } from "../../util/auth";
-import { useNavigate } from "react-router-dom";
-import { getUser } from "../../util/userUtil";
+import { useUser } from "../../util/UserContext";
 
 const HomePage = () => {
-  const [userData, setUserData] = useState(null);
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const fetchUserData = async () => {
-      const data = await getUser();
-      setUserData(data);
-    };
-
-    fetchUserData();
-  }, []);
-
-  const handleLogout = async () => {
-    await logout();
-    navigate("/auth/login");
-  };
-
-  return (
-    <div className="flex justify-center items-center h-screen">
-      {userData ? (
-        <div>
-          <h1>{JSON.stringify(userData.email)}</h1>
-          <button onClick={handleLogout}>Logout</button>
-        </div>
-      ) : (
-        <p>Loading user data...</p>
-      )}
-    </div>
-  );
+  const { user, loading } = useUser();
+  if (loading) {
+    return <div>loading</div>;
+  }
+  return <div>{user.fullName}</div>;
 };
 
 export default HomePage;
