73795db7a58424c0df6fe07b5eb0c28449759b6b
johnnybinh <thebinhnguyen2703@gmail.com>
2024-11-21 22:00:23 +0700

    done video prototypeing

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,14 +5,19 @@ import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "./components/ui/toaster.tsx";
 import { UserProvider } from "./util/UserContext.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const que = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <UserProvider>
-      <BrowserRouter>
-        <App />
-        <Toaster />
-      </BrowserRouter>
-    </UserProvider>
+    <QueryClientProvider client={que}>
+      <UserProvider>
+        <BrowserRouter>
+          <App />
+          <Toaster />
+        </BrowserRouter>
+      </UserProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
